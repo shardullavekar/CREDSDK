6d13a9ee70bb844cf89b467e3435b2e46f54c9c7
Shardul <developershardul@gmail.com>
2018-07-25 17:27:15 +0530

    Added Chatview

--- app/src/main/java/cred/io/sdk/MainActivity.java
+++ app/src/main/java/cred/io/sdk/MainActivity.java
@@ -1,13 +1,105 @@
 package cred.io.sdk;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import chat.Message;
+import chat.ThreadAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ProgressDialog dialog;
+
+    //Recyclerview objects
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter adapter;
+
+    //ArrayList of messages to store the thread messages
+    private ArrayList<Message> messages;
+
+    //Button to send new message on the thread
+    private ImageButton buttonSend;
+
+    //EditText to send new message on the thread
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initViews();
     }
+
+    private void initViews() {
+        recyclerView = findViewById(R.id.recyclerView);
+        buttonSend = findViewById(R.id.buttonSend);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        messages = new ArrayList<>();
+
+        buttonSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMessage();
+            }
+        });
+    }
+
+    private void processMessage(String name, String message, String id) {
+        DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy, HH:mm");
+        String date = df.format(Calendar.getInstance().getTime());
+        Message message1 = new Message(Integer.valueOf(id), message, date, "CRED");
+        messages.add(message1);
+        adapter.notifyDataSetChanged();
+        scrollToBottom();
+    }
+
+    private void scrollToBottom() {
+        adapter.notifyDataSetChanged();
+        if (adapter.getItemCount() > 1)
+            recyclerView.getLayoutManager().smoothScrollToPosition(recyclerView, null, adapter.getItemCount() - 1);
+    }
+
+    private void sendMessage() {
+        final String message = editTextMessage.getText().toString().trim();
+        if (message.equalsIgnoreCase(""))
+            return;
+
+
+        DateFormat df = new SimpleDateFormat("EEE, d MMM yyyy, HH:mm");
+        String date = df.format(Calendar.getInstance().getTime());
+        Message message1 = new Message(101, message, date,"");
+        messages.add(message1);
+        initAdapter();
+        adapter.notifyDataSetChanged();
+
+        scrollToBottom();
+
+        editTextMessage.setText("");
+
+        processMessage("CRED", "Hey there, thanks for reaching out! We are looking into your issue.", "102");
+
+    }
+
+    private void initAdapter() {
+        adapter = new ThreadAdapter(this, messages, 101);
+        recyclerView.setAdapter(adapter);
+    }
+
+
 }
