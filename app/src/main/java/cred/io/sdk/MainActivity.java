bfc972c0121261ab9a2e64c5d263a59e7dddbe18
Shardul <developershardul@gmail.com>
2018-07-26 16:36:19 +0530

    Widgets init code added

--- app/src/main/java/cred/io/sdk/MainActivity.java
+++ app/src/main/java/cred/io/sdk/MainActivity.java
@@ -50,6 +50,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initViews();
         setView();
+        initRating();
+        initSuggestionForm();
+        initYesorno();
     }
 
     private void initViews() {
