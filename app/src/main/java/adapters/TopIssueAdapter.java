951f8ab877d452c063a60aa82ad348c518dc2e83
Shardul <developershardul@gmail.com>
2018-07-28 11:24:32 +0530

    Added POJOs and dummy JSON for Txn History

--- app/src/main/java/adapters/TopIssueAdapter.java
+++ app/src/main/java/adapters/TopIssueAdapter.java
@@ -1,13 +1,52 @@
 package adapters;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+
+import cred.io.sdk.R;
 import pojos.Issues;
 
 public class TopIssueAdapter extends ArrayAdapter<Issues> {
-    public TopIssueAdapter(@NonNull Context context, int resource) {
-        super(context, resource);
+
+    private Context context;
+
+    private ArrayList<Issues> arrayList;
+
+    public TopIssueAdapter(Context context, ArrayList<Issues> issuesArrayList ){
+        super(context, 0, issuesArrayList);
+        this.arrayList = issuesArrayList;
+        this.context = context;
     }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View listItem = convertView;
+        if(listItem == null)
+            listItem = LayoutInflater.from(context).inflate(R.layout.help_list_item,parent,false);
+
+        Issues issue = arrayList.get(position);
+
+        TextView problem_description = listItem.findViewById(R.id.problem_description);
+
+        ImageView problem_image = listItem.findViewById(R.id.leftImage);
+
+        problem_image.setImageURI(Uri.parse(issue.getUrl()));
+
+        problem_description.setText(issue.getDescription());
+
+        return listItem;
+    }
+
+    
 }
--- app/src/main/java/pojos/Issues.java
+++ app/src/main/java/pojos/Issues.java
@@ -1,4 +1,33 @@
 package pojos;
 
 public class Issues {
+
+    private String id;
+    private String description;
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
