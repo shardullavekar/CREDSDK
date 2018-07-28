0e629cf00eeee95337e648922cda760810ff6a26
Shardul <developershardul@gmail.com>
2018-07-28 12:03:24 +0530

    Cardview basic design is ready

--- app/src/main/java/cred/io/sdk/HelpPage.java
+++ app/src/main/java/cred/io/sdk/HelpPage.java
@@ -2,6 +2,9 @@ package cred.io.sdk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import network.ChatAPI;
 
 public class HelpPage extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class HelpPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help_page);
+
+        ChatAPI chatAPI = new ChatAPI(HelpPage.this);
+
+        Toast.makeText(getApplicationContext(), chatAPI.getTxnHistory(), Toast.LENGTH_LONG)
+                .show();
     }
 }
--- app/src/main/java/network/ChatAPI.java
+++ app/src/main/java/network/ChatAPI.java
@@ -1,8 +1,33 @@
 package network;
 
+import android.app.Activity;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 public class ChatAPI {
 
+    Activity activity;
+
+    public ChatAPI(Activity activity) {
+        this.activity = activity;
+    }
 
 
+public String getTxnHistory() {
+    String json = null;
+    try {
+        InputStream is = activity.getAssets().open("getTxn.json");
+        int size = is.available();
+        byte[] buffer = new byte[size];
+        is.read(buffer);
+        is.close();
+        json = new String(buffer, "UTF-8");
+    } catch (IOException ex) {
+        ex.printStackTrace();
+        return null;
+    }
+    return json;
+}
 
 }
